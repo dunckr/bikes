15183e84e9d8259c3df632d773ffeba63c0452ce
Duncan Beaton <d@duncky.com>
2014-05-31 19:46:49 +0100

    add styling and thefts

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,22 +1,13 @@
 $(function() {
 
   var map = L.map('map').setView([55.858, -4.259], 12);
-
-  // L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  //     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  // }).addTo(map);
-		L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-			maxZoom: 18,
-			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-				'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-				'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-			id: 'examples.map-20v6611k'
-		}).addTo(map);
-
+	L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+		id: 'examples.map-20v6611k'
+	}).addTo(map);
 
   var bikeMarker = L.icon({
-      iconUrl: '/images/bikeMarker.png',
-      iconSize: [16, 16]
+      iconUrl: '/images/blackdot.png',
+      iconSize: [8, 8]
   });
 
   function addMarker(geojsonFeature, icon) {
@@ -44,26 +35,56 @@ $(function() {
                 return L.marker(latlng);
             }
         }).addTo(map);
-
       });
     });
   });
 
-  $.getJSON('mock/wards.geojson', function(data) {
-    console.log(data);
-    L.geoJson(data).addTo(map);
+  $.getJSON('mock/bike-thefts.json', function(data) {
 
-    // _.each(data.features, function(point) {
-    //
-    //   var points = point.geometry.coordinates;
-    //   L.layerGroup(points)
-    //       // .addLayer(polyline)
-    //       .addTo(map);
-    //
-    // });
+    _.each(data, function(theft) {
+        var current = theft.stats['12'];
+        var icon = 'blue_circle';
+        var size = [64, 64]
+        if (current > 150) {
+            icon = 'red_circle';
+            size = [96,96];
+        } else if (current > 100) {
+            icon = 'orange_circle';
+        } else if (current > 50) {
+            icon = 'yellow_circle';
+        } else {
+          size = [32,32];
+        }
+        console.log(icon);
 
+        var theftMarker = L.icon({
+            iconUrl: '/images/' + icon + '.png',
+            iconSize: size
+        });
+
+        L.marker(theft.coords, {icon: theftMarker}).addTo(map);
+
+        $('.leaflet-marker-icon').css('opacity', 0.5);
+    });
 
   });
+
+
+  // $.getJSON('mock/wards.geojson', function(data) {
+  //   console.log(data);
+  //   L.geoJson(data).addTo(map);
+  //
+  //   // _.each(data.features, function(point) {
+  //   //
+  //   //   var points = point.geometry.coordinates;
+  //   //   L.layerGroup(points)
+  //   //       // .addLayer(polyline)
+  //   //       .addTo(map);
+  //   //
+  //   // });
+  //
+  //
+  // });
 
   // $.getJSON('mock/strava-stats.json', function(data) {
   //   console.log(data[0]);
