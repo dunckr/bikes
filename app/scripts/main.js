3c7c9c87dfe06c77736d7d3c8952c6f43c04b69e
Duncan Beaton <d@duncky.com>
2014-05-31 14:38:24 +0100

    add cycle paths and racks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,42 @@
 $(function() {
-    // create a map in the "map" div, set the view to a given place and zoom
-  var map = L.map('map').setView([51.505, -0.09], 13);
 
-  // add an OpenStreetMap tile layer
+  var map = L.map('map').setView([55.858, -4.259], 12);
+
   L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map);
 
-  // add a marker in the given location, attach some popup content to it and open the popup
-  L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.')
-      .openPopup();
+  var bikeMarker = L.icon({
+      iconUrl: '/images/bikeMarker.png',
+      iconSize: [16, 16]
+  });
+
+  function addMarker(geojsonFeature, icon) {
+    L.geoJson(geojsonFeature, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng,
+              {icon: bikeMarker});
+        }
+    }).addTo(map);
+  }
+
+  $.getJSON('mock/cycle-racks.geojson', function(data) {
+    _.each(data.features, function(point) {
+      addMarker(point, bikeMarker);
+    });
+  });
+
+$.getJSON('mock/cycling-routes.geojson', function(data) {
+  _.each(data.features, function(points) {
+
+    L.geoJson(points, {
+        pointToLayer: function (feature, latlng) {
+            return L.marker(latlng);
+        }
+    }).addTo(map);
+
+  });
+});
+
+
 });
