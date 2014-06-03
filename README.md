554692abddd152de1bdc36593c13f1876dc60753
Duncan Beaton <d@duncky.com>
2014-06-03 21:11:46 +0100

    add img to readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Glasgow Bike Data Visualisation
 
 [For Glasgow Transport Hackathon](http://open.glasgow.gov.uk/hackathon/hackathon-no4/)
+
+![Map](https://raw.githubusercontent.com/dunckr/bikes/master/app/images/map.png)
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,31 @@
 $(function() {
 
   var map = L.map('map').setView([55.858, -4.259], 12);
-	L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-		id: 'examples.map-20v6611k'
-	}).addTo(map);
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
+    id: 'examples.map-20v6611k'
+  }).addTo(map);
 
   $('.leaflet-control').hide();
   $('.leaflet-control-attribution').hide();
 
 
   var bikeMarker = L.icon({
-      iconUrl: '/images/blackdot.png',
-      iconSize: [8, 8]
+    iconUrl: '/images/blackdot.png',
+    iconSize: [8, 8]
   });
 
   function addMarker(geojsonFeature, icon) {
     L.geoJson(geojsonFeature, {
-        pointToLayer: function (feature, latlng) {
-            return L.marker(latlng,
-              {icon: bikeMarker});
-        }
+      pointToLayer: function(feature, latlng) {
+        return L.marker(latlng, {
+          icon: bikeMarker
+        });
+      }
     }).addTo(map);
   }
 
   $('#opacity').click(function() {
-      $('.leaflet-marker-icon').css('opacity', 0.2);
+    $('.leaflet-marker-icon').css('opacity', 0.2);
   });
 
   $('#racks').click(function() {
@@ -39,9 +40,9 @@ $(function() {
     $.getJSON('mock/cycling-routes.geojson', function(data) {
       _.each(data.features, function(points) {
         L.geoJson(points, {
-            pointToLayer: function (feature, latlng) {
-                return L.marker(latlng);
-            }
+          pointToLayer: function(feature, latlng) {
+            return L.marker(latlng);
+          }
         }).addTo(map);
       });
     });
@@ -50,28 +51,30 @@ $(function() {
   $('#thefts2').click(function() {
     $.getJSON('mock/bike-thefts.json', function(data) {
       _.each(data, function(theft) {
-          var current = theft.stats['12'];
-          var icon = 'blue_circle';
-          var size = [64, 64]
-          if (current > 150) {
-              icon = 'red_circle';
-              size = [96,96];
-          } else if (current > 100) {
-              icon = 'orange_circle';
-          } else if (current > 50) {
-              icon = 'yellow_circle';
-          } else {
-            size = [32,32];
-          }
+        var current = theft.stats['12'];
+        var icon = 'blue_circle';
+        var size = [64, 64]
+        if (current > 150) {
+          icon = 'red_circle';
+          size = [96, 96];
+        } else if (current > 100) {
+          icon = 'orange_circle';
+        } else if (current > 50) {
+          icon = 'yellow_circle';
+        } else {
+          size = [32, 32];
+        }
 
-          var theftMarker = L.icon({
-              iconUrl: '/images/' + icon + '.png',
-              iconSize: size
-          });
+        var theftMarker = L.icon({
+          iconUrl: '/images/' + icon + '.png',
+          iconSize: size
+        });
 
-          L.marker(theft.coords, {icon: theftMarker}).addTo(map);
+        L.marker(theft.coords, {
+          icon: theftMarker
+        }).addTo(map);
 
-          $('.leaflet-marker-icon').css('opacity', 0.5);
+        $('.leaflet-marker-icon').css('opacity', 0.5);
       });
     });
   });
@@ -80,42 +83,42 @@ $(function() {
     $.getJSON('mock/glasgow-wards.json', function(data) {
       console.log(data);
       _.each(data, function(ward) {
-        L.geoJson(ward,
-      {
+        L.geoJson(ward, {
           style: function(feature) {
-              var color = feature.properties.Color;
-              var hex = '#049BD2';
-              var opacity = 1.0
-              if (color > 150) {
-                  hex = '#FF2B1D';
-                  opacity = 0.1;
-              } else if (color > 100) {
-                  hex = '#FF8230';
-              } else if (color > 50) {
-                  hex = '#F3D702';
-              } else {
-              }
-              return {
-                  "color": hex,
-                  "weight": 1,
-                  "opacity": opacity
-              }
+            var color = feature.properties.Color;
+            var hex = '#049BD2';
+            var opacity = 1.0
+            if (color > 150) {
+              hex = '#FF2B1D';
+              opacity = 0.1;
+            } else if (color > 100) {
+              hex = '#FF8230';
+            } else if (color > 50) {
+              hex = '#F3D702';
+            } else {}
+            return {
+              "color": hex,
+              "weight": 1,
+              "opacity": opacity
+            }
           }
-      }).addTo(map);
+        }).addTo(map);
       });
     });
   });
 
   var accomodationMarker = L.icon({
-      iconUrl: '/images/home.png',
-      // iconUrl: '/images/blackdot.png',
-      iconSize: [32, 32]
+    iconUrl: '/images/home.png',
+    // iconUrl: '/images/blackdot.png',
+    iconSize: [32, 32]
   });
 
   $('#accomodation').click(function() {
     $.getJSON('mock/accomodation.json', function(accomodations) {
       _.each(accomodations, function(accomodation) {
-          L.marker(accomodation.coords, {icon: accomodationMarker}).addTo(map);
+        L.marker(accomodation.coords, {
+          icon: accomodationMarker
+        }).addTo(map);
       });
     });
   });
